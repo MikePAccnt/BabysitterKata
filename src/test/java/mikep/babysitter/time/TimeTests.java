0b09faaef51d04647ff2b190f6cfc49bb8747d9c
MikeSchoolAccnt <purcell.14@wright.edu>
2018-10-08 20:27:21 -0400

    Fix TimeTests method names
    For got to rename method names when copy pasting.

--- src/test/java/mikep/babysitter/time/TimeTests.java
+++ src/test/java/mikep/babysitter/time/TimeTests.java
@@ -9,7 +9,7 @@ public class TimeTests {
     //region isValidTests
     @DisplayName("Test valid time: StartTime No earlier than 5:00PM")
     @Test
-    public void testIsValidTime(){
+    public void testIsValidTimeNoEarlier(){
 
         //StartTime, BedTime, EndTime
         Time time = new Time("4:00PM", "8:00PM" ,"7:00PM");
@@ -19,7 +19,7 @@ public class TimeTests {
 
     @DisplayName("Test valid time: EndTime No later than 4:00AM")
     @Test
-    public void testIsValidTime(){
+    public void testIsValidTimeNoLater(){
 
         //StartTime, BedTime, EndTime
         Time time = new Time("6:00PM", "8:00PM" ,"5:00AM");
@@ -30,7 +30,7 @@ public class TimeTests {
 
     @DisplayName("Test valid time: Between 5:00PM and 4:00AM")
     @Test
-    public void testIsValidTime(){
+    public void testIsValidTimeBetween(){
 
         //StartTime, BedTime, EndTime
         Time time = new Time("6:00PM", "8:00PM" ,"3:00AM");
@@ -41,7 +41,7 @@ public class TimeTests {
 
     @DisplayName("Test valid time: StartTime At 5:00PM")
     @Test
-    public void testIsValidTime(){
+    public void testIsValidTimeAt5PM(){
 
         //StartTime, BedTime, EndTime
         Time time = new Time("5:00PM", "8:00PM" ,"3:00AM");
@@ -52,7 +52,7 @@ public class TimeTests {
 
     @DisplayName("Test valid time: EndTime At 4:00AM")
     @Test
-    public void testIsValidTime(){
+    public void testIsValidTimeAt4AM(){
 
         //StartTime, BedTime, EndTime
         Time time = new Time("6:00PM", "8:00PM" ,"4:00AM");
@@ -63,7 +63,7 @@ public class TimeTests {
 
     @DisplayName("Test valid time: BedTime later than EndTime")
     @Test
-    public void testIsValidTime(){
+    public void testIsValidTimeBedTimeLaterEndTime(){
 
         //StartTime, BedTime, EndTime
         Time time = new Time("6:00PM", "8:00PM" ,"7:00PM");
@@ -74,7 +74,7 @@ public class TimeTests {
 
     @DisplayName("Test valid time: BedTime earlier than StartTime")
     @Test
-    public void testIsValidTime(){
+    public void testIsValidTimeBedTimeEarlierStartTime(){
 
         //StartTime, BedTime, EndTime
         Time time = new Time("7:00PM", "6:00PM" ,"9:00PM");
